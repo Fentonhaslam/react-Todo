f1773d45e53d3afd2495b46dad96e859fdd5fd0c
fentonhaslam <fentonhaslam@gmail.com>
2019-10-28 16:29:20 +0000

    added deleted button and changing state for completed todo

--- src/App.js
+++ src/App.js
@@ -8,26 +8,45 @@ class App extends React.Component {
     todos: [
       {
         id: 1,
-        title: 'Apply to a job you are interested in',
+        title: ' Apply to a job you are interested in',
         completed: false
       },
       {
         id: 2,
-        title: 'Start a new project',
+        title: ' Start a new project',
         completed: false
       },
       {
         id: 3,
-        title: 'Read an hour of your book everyday this week',
+        title: ' Read an hour of your book everyday this week',
         completed: false
       },
     ]
   }
+  // Toggle complete
+  markComplete = (id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed
+
+        }
+        return todo;
+      })
+    });
+  }
+
+  // delete todo
+
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+  }
+
   render() {
     console.log(this.state.todos)
     return (
       <div className="App">
-        <Todos todos={this.state.todos} />
+        <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
       </div>
     );
   }
